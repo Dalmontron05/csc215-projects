eb504962a25ae6156dba79834003b98d48ad00b0
DALJEET SINGH <DSINGH83800@uat.edu>
2024-05-15 11:39:30 -0700

    initial commit

---
+++ Assignment1p1/Assignment1p1.cpp
@@ -0,0 +1,15 @@
+/*
+    Daljeet Singh
+    05/15/2024
+*/
+
+#include <iostream>
+//using std::cout;
+//using std::endl;
+using namespace std;
+int main()
+{
+    cout << "Game Over!" << endl;
+    return 0;
+}
+
