a93bafca5c1c5fa5328f7d5eedd7db92d55f61e3
Dalmontron05 <vexr2dts85338@gmail.com>
2024-07-16 19:57:46 -0700

    finished 5.1

--- uat-classroom/main.cpp
+++ uat-classroom/main.cpp
@@ -38,23 +38,31 @@ int main()
     {
         // variables
         string guessedProfessor, chosenProfessor;
-        int chosenProfessorIndex;
+        int chosenProfessorIndex, guessedProfessorLocationIndex, chosenProfessorLocationIndex;
         char askToRetake = 'n';
 
         // prints all professors
-        cout << "Hello! Here are some of the professors you might have here at UAT:\n";
+        cout << "Hello! Here are some of the professors you might have here at UAT:" << endl;
         for (int i = 0; i < 10; i++)
         {
             cout << "• " << professors[i] << endl;
         }
 
         // asks user to guess a professor
-        cout << "Suprise! Today you're going to meet one of these professors. Who do you think it'll be?: ";
+        cout << "Suprise! Today you're going to meet one of these professors. Who do you think it'll be?: " << endl << endl;
         getline(cin, guessedProfessor);
+        cin.ignore(1000, '\n');
+
+        // asks user to guess which classroom the professor is in
+        cout << "We've shuffled the professors into 20 different classes. Where do you think " << guessedProfessor << " is? (enter a number between 1 and 20): ";
+        cin >> guessedProfessorLocationIndex;
 
         // generates a random number between 0 and 9. this interger will represent the random professor
         srand(time(0));
         chosenProfessorIndex = rand() % 10;
+
+        // generates a random number between 1 and 20. this integer will represent the room number the professor is in. 
+        chosenProfessorLocationIndex = rand() % 20 + 1;
 
         // gives a string to the chosen professor index
         chosenProfessor = professors[chosenProfessorIndex];
@@ -68,24 +76,34 @@ int main()
                 // checks if that name is the chosenProfessor
                 if (guessedProfessor == chosenProfessor)
                 {
-                    cout << "That's right!.\n";
+                    cout << "You correctly guessed the professors name" << endl;
                 }
                 // if not, tells user who the correct professor was
                 else
                 {
-                    cout << "Nope! Today you'll be seeing-"<< endl;
+                    cout << "You did not correctly guess the professors name" << endl;
                 }
             }
+        }
+
+        // checks if guessed professor location matches chosen professor location
+        if (guessedProfessorLocationIndex == chosenProfessorLocationIndex)
+        {
+            cout << "You correctly guessed which classroom the professor is in" << endl;
+        }
+        else
+        {
+            cout << "You did not correctly guessed which classroom the professor is in" << endl;
         }
         
         // plays a bell sound when the chosen professor is here.
         Beep(750, 100);
-        cout << "Oh! They're here! That signifies this part of the tour. Enjoy your time with " << chosenProfessor << "!\n";
+        cout << "Oh! That signifies this part of the tour. Enjoy your time with " << chosenProfessor << " in classroom " << chosenProfessorLocationIndex << "!" << endl;
 
         // asks user if they want to try again
         cout << "Do you want to run the simulation again? (y/n): ";
         cin >> askToRetake;
-        cin.ignore(1, '\n'); // why on gods green earth is this cin and getline bullshit so sensitive
+        cin.ignore(1000, '\n');
 
         if (askToRetake == 'y')
         {
